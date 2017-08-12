6a067a5e3e36d438ba6abfd37778af043cf6d7a3
maolion <maolion.j@gmail.com>
2017-08-13 02:29:41 +0800

    Add home and blog simple pages

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
+import { BlogView } from './pages/blog/blog.view';
+import { HomeView } from './pages/home/home.view';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeView,
+  },
+  {
+    path: 'blog',
+    component: BlogView,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [
+    AppComponent,
+    HomeView,
+    BlogView,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/client/app/pages/blog/blog.view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'm-page.blog',
+  templateUrl: './blog.view.html',
+  styleUrls: ['./blog.view.less'],
+})
+export class BlogView {
+}
